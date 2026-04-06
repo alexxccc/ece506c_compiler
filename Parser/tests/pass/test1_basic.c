022ec810836e09f430ce699439bef362a2bcdf5c
Alex <62362101+alexxccc@users.noreply.github.com>
2026-04-06 16:38:39 -0700

    add passing tests

---
+++ Parser/tests/pass/test1_basic.c
@@ -0,0 +1,9 @@
+void setup() {
+    int x = 5;
+    bool ready = true;
+}
+
+void loop() {
+    int y;
+    y = 10;
+}
---
+++ Parser/tests/pass/test2_serial_delay.c
@@ -0,0 +1,9 @@
+void setup() {
+    Serial.println("Starting");
+    delay(1000);
+}
+
+void loop() {
+    Serial.print("Looping");
+    delay(500);
+}
