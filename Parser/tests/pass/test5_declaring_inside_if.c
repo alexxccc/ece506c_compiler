9abc524a0afba1723ceeab71019c86c6008551d4
Alex <62362101+alexxccc@users.noreply.github.com>
2026-04-20 17:14:13 -0700

    add test

---
+++ Parser/tests/pass/test5_declaring_inside_if.c
@@ -0,0 +1,19 @@
+void setup() {
+    int x = 3;
+
+    if (x < 5) {
+        int y = x + 1;
+    } else {
+        x = x - 1;
+    }
+}
+
+void loop() {
+    bool flag = false;
+
+    if (!flag) {
+        flag = true;
+    } else {
+        flag = false;
+    }
+}
